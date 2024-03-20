4e45613ccf1944fd92b77c31a730dc200287b88e
Danton Vital Ponce <d.ponce@AVIVATEC.COM.BR>
2024-03-20 19:53:07 -0300

    Setting the variables style and starting to develop the login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
+import { LoginComponent } from './users/page/login/login.component';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full'
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    }
+];
